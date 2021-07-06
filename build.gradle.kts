7045648e115efb2737a43d7cbc69b12c533c7243
David Madouros <david.madouros@rxrevu.com>
2021-07-06 16:45:37 -0600

    Add ability to create space

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("com.h2database:h2:1.4.197")
+	implementation("org.liquibase:liquibase-core")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
---
+++ src/main/kotlin/com/manning/apisecurityinaction/controller/SpaceController.kt
@@ -0,0 +1,49 @@
+package com.manning.apisecurityinaction.controller
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.jdbc.core.RowMapper
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.util.LinkedMultiValueMap
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RestController
+import java.sql.ResultSet
+import javax.servlet.http.HttpServletResponse
+
+
+@RestController
+class SpaceController(
+    @Autowired val jdbcTemplate: JdbcTemplate,
+    @Autowired val objectMapper: ObjectMapper,
+) {
+
+    @GetMapping(
+        "/spaces",
+        consumes = [MediaType.APPLICATION_JSON_VALUE],
+        produces = [MediaType.APPLICATION_JSON_VALUE]
+    )
+    @Transactional
+    fun createSpace(@RequestBody body: String): ResponseEntity<Map<String, Any>> {
+        val json = objectMapper.readTree(body)
+        val spaceName = json["name"]
+        val owner = json["owner"]
+
+        val spaceId = jdbcTemplate.query(
+            "SELECT NEXT VALUE FOR space_id_seq"
+        ) { resultSet: ResultSet, rowIndex: Int ->
+            resultSet.getInt(1)
+        }.first()
+        jdbcTemplate.execute(
+            "INSERT INTO spaces(space_id, name, owner) VALUES(${spaceId}, '${spaceName}', '${owner}');"
+        )
+        val uri = "/spaces/${spaceId}"
+        val responseBody = mapOf("name" to spaceName, "uri" to uri)
+        val headers = LinkedMultiValueMap<String, String>().apply {add("Location", uri)}
+        return ResponseEntity(responseBody, headers, HttpStatus.CREATED)
+    }
+}
